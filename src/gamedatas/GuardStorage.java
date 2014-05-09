02bf8611f857cf7a62a792fcd1a1eb74a5f154b0
Hadrien Mittoux <hadrien.mittoux@gmail.com>
2014-05-09 16:03:25 +0200

    [+]création storage guards [*]changement de la fonction de modification du tableau de nombre d'items

--- src/gamedatas/GuardStorage.java
+++ src/gamedatas/GuardStorage.java
@@ -1,23 +1,81 @@
 package gamedatas;
 
-
 import items.Item;
 
 public class GuardStorage
 {
-
+    /* ---------------------- START DECLARATIONS ---------------------- */
+    
+    /**
+     * declaration of an array composed with Item objects.
+     */
+    Item[] storeItems;
+    /**
+     * declaration of an array composed with integers.
+     */
+    int[] storeCapacity;
+    /* ---------------------- END DECLARATIONS ---------------------- */
+    
+    /* ---------------------- START CONSTRUCTOR(S) ---------------------- */
+    
+    public void guardStorage()
+    {
+        this.storeItems = new Item[Store.NUMBER_OF_ITEMS_AVAILABLE];
+        this.storeCapacity = new int[Store.NUMBER_OF_ITEMS_AVAILABLE];
+    }
+    
+    /* ---------------------- END CONSTRUCTOR(S) ---------------------- */
+    
+    /* ---------------------- START FUNCTION(S) ---------------------- */
+     
     /**
      * when a guard takes an item.
      *
      * @param item what the guards want for an item.
      * @param quantity how many items he wants to take.
-     * @return an array containing the items the guard took.
      */
-    public Item[] takeItem(Item item, Integer quantity)
+    
+    public void takeItem(String item, Integer quantity)
     {
-        Item[] itemArray;
-        itemArray = new Item[quantity];
-        return itemArray;
+        this.storeCapacity[getPositionItemFromName(item)]-=quantity;
     }
+    
+     /**
+     *
+     * get the position in the array with the item name.
+     *
+     * @param itemName name of the item to search.
+     * @return position of the item in the array.
+     */
+    
+    public int getPositionItemFromName(String itemName)
+    {
+        int itemPosition = 0;
+        String currentItemName = "";
+        for (int i = 0; i < Store.NUMBER_OF_ITEMS_AVAILABLE && !currentItemName.equals(itemName); i++) {
+            currentItemName = this.storeItems[i].getItemName();
+            itemPosition = i;
+        }
+        return itemPosition;
+    }
+       
+    /* ---------------------- END FUNCTION(S) ---------------------- */
+    
+    /* ---------------------- START GETTERS & SETTERS ---------------------- */
+    
+    public int[] getStoreCapacity()
+    {
+        return this.storeCapacity;
+    }
+
+    public Item[] getStoreItems()
+    {
+        return this.storeItems;
+    }    
+    
+    /* ---------------------- END GETTERS AND SETTERS ---------------------- */    
+
+
+
 
 }
